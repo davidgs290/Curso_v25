8860de60eab7e2333c424b52e702b0ce665e627d
davidgs290 <davidgs290@hotmail.com>
2025-08-07 11:29:51 -0600

    Add files via upload

---
+++ Formulario_Actividad.m
@@ -0,0 +1,136 @@
+%[text] # Formulario Actividad de Enseñanza
+%[text] ## ¿Cuál es el título de su actividad?
+%[text] <u>Respuesta:</u>
+%[text] <u>Metodo de punto fijo</u>
+%[text] 
+%[text] 
+%%
+%[text] ## ¿Qué tipo de actividad le gustaría desarrollar?
+"two" %[control:dropdown:2c77]{"position":[1,6]}
+%%
+%[text] ## ¿Cuál es el nivel educativo de los estudiantes?
+"two" %[control:dropdown:1158]{"position":[1,6]} %[output:1fbb31ea]
+%%
+%[text] ## ¿Cuál es el área de conocimiento?
+"Matemáticas" %[control:dropdown:633f]{"position":[1,14]} %[output:329dd9ae]
+%%
+%[text] ## ¿Qué duración tendrá la actividad?
+"two" %[control:dropdown:427b]{"position":[1,6]} %[output:72319314]
+%%
+%[text] ## ¿Qué pre-requisitos se necesitan?
+%[text] Por favor enliste los conocimientos que los alumnos necesitan para poder realizar esta actividad:
+"terorema del valor intermedio" %[control:editfield:64e0]{"position":[1,32]}
+
+%%
+%[text] ## ¿Qué tan adecuado es su actividad para ser usado en un entorno en línea?
+"actividad" %[control:dropdown:6068]{"position":[1,12]}
+%%
+%[text] ## Resumen
+%[text] Por favor, proporcione una breve descripción de su actividad o tarea y sus resultados. Asegúrese de incluir palabras clave para ayudar otros a encontrar sus materiales utilizando nuestras funciones de búsqueda/navegación.
+%[text] <u>Respuesta:</u>
+%[text] <u>Revisar los conceptos en el teorema del valor intermedio, palntear el algoritmo para el metodod de la biseccion, resolver un problema de una ecuacion con una incognita mediante un grafico y despues implementar un codigo de matlab que contruya una tabla que muestre el proceso de la biseccion para la solucion de ecuaciones con una incognita-</u>
+%[text] 
+%[text] 
+%%
+%[text] ## Contexto
+%[text] Este texto debería ayudar a otros docentes a comprender las situaciones de enseñanza para las cuales esta actividad es apropiada.
+%[text] Los componentes importantes del contexto incluyen por ejemplo nivel educativo tamaño de la clase, tipo de institución, habilidades técnicas y la experiencia con MATLAB que los estudiantes deben haber dominado antes del comienzo de la actividad. 
+%[text] <u>Respuesta:</u>
+%[text] <u>Es un curso de metodos numericos pero se usan estos como pretexto para introducir el uso del matlab y comprender la programacion. son estudiantes de segundo año de licenciatura del area de ingenierias, es el primer contacto que tienen con el matlab.</u>
+%[text] 
+%[text] 
+%%
+%[text] ##  ¿Qué conceptos, contenido y/o habilidades deben aprender los estudiantes de esta actividad?
+%[text] <u>Respuesta:</u>
+%[text] <u>Deben comprender y operar el algortimo de la biseccion para la solucion de ecuaciones con una incognita</u>
+%[text] <u>deben de crear codigos de matlab para subrutinas y funciones</u>
+%[text] <u>deben de progrmar pequeños codigos en matlab</u>
+%[text] 
+%%
+%[text] ## Descripción de la Actividad y Materiales de Enseñanza
+%[text] Esta sección debe describir la mecánica de la actividad y todos los materiales necesarios para implementarla (o enlaces y referencias a esos materiales). Por favor, incluya una descripción de cómo se utiliza MATLAB en la actividad, si la actividad se puede realizar utilizando otro software y, de ser así, por qué eligió utilizar MATLAB.
+%[text] <u>Respuesta:</u>
+%[text] <u>La actividad tiene un primer momento donde los alumnos se enfrentan a un problema con una ecuacion con una incognita y se les pide resolverla, cabe indicar que es una ecuacnion que no es posible resolverla por despejes o factorizaciones.</u>
+%[text] <u>el sigueinte momento se les pide que realicen una graica de la ecuacion para determinar la solucin por inspeccion en ella, (se les explica como introducir vectores en matlab y como realizar graficas</u>
+%[text] 
+%[text] <u>el siguiente se realia una exposicion sobre las diferencias entre una subrutina y una funcion en matlab</u>
+%[text] 
+%[text] <u>luego se les pide que programen una fucnion con la ecuacion a resolver.</u>
+%[text] 
+%[text] <u>una vez teniendo la funcion de procede a crear una subrutina que forme una tabla con el desarrollo del metodo de la biseccion.</u>
+%[text] 
+%[text] <u>se lesaccigna un problema diferente a cada a lumno parafavorecer quetrabaje y se revisa los resultados obtenidos contra las respuesta</u> 
+%[text] 
+%[text] 
+%[text] 
+%[text] 
+%[text] 
+%[text] 
+%%
+%[text] ## Materiales de Soporte
+%[text] Aquí puede cargar archivos de soporte si es el caso
+%[text] el material de soporte un rpogram qe asigna a cada aluno un problema con datos diferentes para favorecer que trabajen de forma individual en estas tareas sencillas 
+%[text] 
+%[text] 
+%%
+%[text] ## Notas para los Educadores usando la Actividad
+%[text] Esta sección debe incluir notas y consejos para los instructores que podrían utilizar la actividad.
+%[text] es importante siempe tener un programa que sea capaz de resolver el problema con las parametrizaciones que se programaron en la asignacion de los problemas individuales para que sea posible revisr si llegan al resultado
+%[text] 
+%[text] 
+%[text] 
+%%
+%[text] ## Evaluación
+%[text] Describa brevemente cómo determina si los estudiantes han alcanzado los objetivos de esta tarea o actividad.
+%[text] <u>Respuesta:</u>
+%[text] <u>los alumnos obtienen una nota aprobatoria si entregan los codigos que puedan evaluar la ecuacion a resolver y para generar la tabla que muestre el proceso de la biseccion</u>
+%[text] 
+%[text] 
+%%
+%[text] ## Referencias y Recursos Adicionales
+%[text]  Esta sección debe incluir referencias y enlaces a recursos en línea que discutan la actividad específica o que apoyen a los docentes y/o estudiantes que utilicen la actividad.
+%[text] 
+%[text] las referencias que uso son el libro de texto 
+%[text] 
+%[text] 
+%[text] 
+%%
+%[text] ## Plan de Acción y Notas 
+%[text] Utiliza esta sección para trazar tu camino a seguir en la creación de nuevos materiales o en la implementación de lo que has aprendido en el taller. 
+%[text] - ¿Qué cambios quieres implementar y dónde? ¿Herramientas? ¿Estrategias? 
+%[text] - ¿Cuál es tu cronograma para el desarrollo e implementación? ¿Cómo evaluarás tus cambios? 
+%[text] - Notas Adicionales:  \
+%[text] me gustaria que la asignacion de los problemas se haga directamente en el matlab ya que ne este momento lo hago a travez de un archivo en MyOpenMAthe que les asigna los problemas 
+
+%[appendix]{"version":"1.0"}
+%---
+%[metadata:view]
+%   data: {"layout":"inline","rightPanelPercent":40}
+%---
+%[control:dropdown:2c77]
+%   data: {"defaultValue":"\"one\"","itemLabels":["Conjunto de problemas","Actividad para realizar en el aula","Actividad de laboratorio","Proyecto","Actividad de Campo","Tarea Escrita"],"items":["\"one\"","\"Actividad para realizar en el aula\"","\"two\"","\"Tarea\"","\"Actividad de Campo\"","\"Tarea Escrita\""],"label":"Tipo de Actividad","run":"Nothing"}
+%---
+%[control:dropdown:1158]
+%   data: {"defaultValue":"\"one\"","itemLabels":["Escuela Secundaria","Primeros dos años de licenciatura","Últimos dos años de licenciatura","Posgrado\/Profesional","Informal","Público en general"],"items":["\"one\"","\"two\"","\"Últimos dos años de licenciatura\"","\"Posgrado\/Profesional\"","\"Informal\"","\"Público en general\""],"label":"Nivel Educativo","run":"Section"}
+%---
+%[control:dropdown:633f]
+%   data: {"defaultValue":"\"one\"","itemLabels":["Biología","Química","Ciencias de la Computación","Ingeniería","Ciencias Ambientales","Geografía","Ciencias de la Tierra","Ciencias de la Salud","Matemáticas","Física y Astronomía","Otro"],"items":["\"one\"","\"two\"","\"Ciencias de la Computación\"","\"Ingeniería\"","\"Ciencias Ambientales\"","\"Geografía\"","\"Ciencias de la Tierra\"","\"Ciencias de la Salud\"","\"Matemáticas\"","\"Física y Astronomía\"","\"Otro\""],"label":"Área de Conocimiento","run":"Section"}
+%---
+%[control:dropdown:427b]
+%   data: {"defaultValue":"\"one\"","itemLabels":["Rápida (<10 min)","Parte de una clase (10-40 min)","Clase completa (40 - 70 min)","Laboratorio (70 - 180 min)","Extendida (>180 min)"],"items":["\"one\"","\"two\"","\"Clase completa (40 - 70 min)\"","\"Laboratorio (70 - 180 min)\"","\"Extendida (>180 min)\""],"label":"Duración","run":"Section"}
+%---
+%[control:editfield:64e0]
+%   data: {"defaultValue":"\"Pre-requisitos: \"","label":"Pre-requisitos","run":"Nothing","valueType":"String"}
+%---
+%[control:dropdown:6068]
+%   data: {"defaultValue":"\"one\"","itemLabels":["Listo para usar en línea","Adaptable para usar en línea","No apto para usar en línea"],"items":["\"one\"","\"actividad\"","\"se\""],"label":"entorno","run":"Nothing"}
+%---
+%[output:1fbb31ea]
+%   data: {"dataType":"textualVariable","outputData":{"name":"ans","value":"\"two\""}}
+%---
+%[output:329dd9ae]
+%   data: {"dataType":"textualVariable","outputData":{"name":"ans","value":"\"Matemáticas\""}}
+%---
+%[output:72319314]
+%   data: {"dataType":"textualVariable","outputData":{"name":"ans","value":"\"two\""}}
+%---
